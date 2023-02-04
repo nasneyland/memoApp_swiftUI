2fb6c8d6fc339f88d2e472300cf05e9f4442c9a5
Najin Shin <63686155+skwls2087@users.noreply.github.com>
2023-02-04 18:05:35 +0900

    project: 프로젝트 생성

---
+++ swiftUImemo/swiftUImemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUImemoApp.swift
+//  swiftUImemo
+//
+//  Created by najin on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct swiftUImemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
