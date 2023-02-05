1ad036c87f560145d80df331a04416181cffaa82
Najin Shin <63686155+skwls2087@users.noreply.github.com>
2023-02-05 13:58:53 +0900

    feat: 새메모 뷰 연결하기

--- swiftUImemo/View/MainListView.swift
+++ swiftUImemo/View/MainListView.swift
@@ -11,6 +11,8 @@ struct MainListView: View {
     //@EnvironmentObject : 공유데이터 목록 중 데이터저장소 불러오기
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer: Bool = false
+    
     var body: some View {
         
         // Shift + Command + A 눌러서 Embed 가능 (상위 속성 선언)
@@ -21,6 +23,18 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                Button {
+                    // 버튼 클릭 이벤트
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showComposer) {
+                ComposeView() // 뷰 카드모달 형태로 띄어주기
+            }
+
         }
     }
 }
